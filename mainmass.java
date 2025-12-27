9fb869626d6a4a5963808db427f6f8bcb9f67e4d
uncoree <wavylins@gmail.com>
2025-12-27 05:28:06 +0300

    mainmass.java

--- mainmass.java
+++ mainmass.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[10]; // массив из 10 чисел
+        int[] arr = new int[10]; 
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
@@ -36,3 +36,4 @@ public class Main {
         System.out.println(lessZero);
     }
 }
+
